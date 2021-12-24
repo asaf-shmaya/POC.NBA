d8d166137aa8b7c4ec1d799823173398c24bff83
Asaf Shmaya <sh_asaf@hotmail.com>
2021-12-24 14:24:13 +0200

    Added event listener for click on apply

---
+++ NBA.Web/Scripts/nba.js
@@ -0,0 +1,8 @@
+﻿// Passing a named function instead of an anonymous function.
+function readyFn(jQuery) {
+    // Code to run when the document is ready.
+    $("#divBtnApply").click(() => { alert("#divBtnApply" + " click!") });
+}
+
+$(document).ready(readyFn);
+
