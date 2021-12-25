5818bee2a3899cf0abfe4798a35ffb3856f219d4
Asaf Shmaya <sh_asaf@hotmail.com>
2021-12-25 19:34:16 +0200

    Added mock data Added css loader + js logic to show / hide Added html markup for loader

--- NBA.Web/Scripts/nba.js
+++ NBA.Web/Scripts/nba.js
@@ -1,5 +1,309 @@
 ﻿var app = [];
 app.data = [];
+app.data.mock =
+    [
+        {
+            "personId": 1627767,
+            "teamId": "12326",
+            "firstName": "Cheick",
+            "lastName": "Diallo",
+            "temporaryDisplayName": "Diallo, Cheick",
+            "heightMeters": "2.06",
+            "careerSummary": {
+                "fgp": "58.5",
+                "ppg": "5.5",
+                "rpg": "4.6",
+                "apg": "0.4",
+                "bpg": "0.5"
+            },
+            "teamSitesOnly": {
+                "posFull": "Forward"
+            },
+            "team": {
+                "teamId": "12326",
+                "africa": null,
+                "config": {
+                    "primaryColor": "#000000"
+                }
+            },
+            "africa": {
+                "nickname": null,
+                "confName": null,
+                "country": "Mali"
+            }
+        },
+        {
+            "personId": 202355,
+            "teamId": "12327",
+            "firstName": "Hassan",
+            "lastName": "Whiteside",
+            "temporaryDisplayName": "Whiteside, Hassan",
+            "heightMeters": "2.13",
+            "careerSummary": {
+                "fgp": "57.4",
+                "ppg": "13.4",
+                "rpg": "11.4",
+                "apg": "0.6",
+                "bpg": "2.3"
+            },
+            "teamSitesOnly": {
+                "posFull": "Center"
+            },
+            "team": {
+                "teamId": "12327",
+                "africa": null,
+                "config": {
+                    "primaryColor": "#000000"
+                }
+            },
+            "africa": {
+                "nickname": null,
+                "confName": null,
+                "country": "USA"
+            }
+        },
+        {
+            "personId": 201580,
+            "teamId": "12327",
+            "firstName": "JaVale",
+            "lastName": "McGee",
+            "temporaryDisplayName": "McGee, JaVale",
+            "heightMeters": "2.13",
+            "careerSummary": {
+                "fgp": "57",
+                "ppg": "8",
+                "rpg": "5.1",
+                "apg": "0.4",
+                "bpg": "1.6"
+            },
+            "teamSitesOnly": {
+                "posFull": "Center"
+            },
+            "team": {
+                "teamId": "12327",
+                "africa": null,
+                "config": {
+                    "primaryColor": "#000000"
+                }
+            },
+            "africa": {
+                "nickname": null,
+                "confName": null,
+                "country": "USA"
+            }
+        },
+        {
+            "personId": 1628381,
+            "teamId": "12327",
+            "firstName": "John",
+            "lastName": "Collins",
+            "temporaryDisplayName": "Collins, John",
+            "heightMeters": "2.08",
+            "careerSummary": {
+                "fgp": "56.6",
+                "ppg": "14.6",
+                "rpg": "8.4",
+                "apg": "1.6",
+                "bpg": "0.9"
+            },
+            "teamSitesOnly": {
+                "posFull": "ForwardCenter"
+            },
+            "team": {
+                "teamId": "12327",
+                "africa": null,
+                "config": {
+                    "primaryColor": "#000000"
+                }
+            },
+            "africa": {
+                "nickname": null,
+                "confName": null,
+                "country": "USA"
+            }
+        },
+        {
+            "personId": 101133,
+            "teamId": "12326",
+            "firstName": "Ian",
+            "lastName": "Mahinmi",
+            "temporaryDisplayName": "Mahinmi, Ian",
+            "heightMeters": "2.11",
+            "careerSummary": {
+                "fgp": "53.7",
+                "ppg": "5",
+                "rpg": "4.3",
+                "apg": "0.5",
+                "bpg": "0.7"
+            },
+            "teamSitesOnly": {
+                "posFull": "Center"
+            },
+            "team": {
+                "teamId": "12326",
+                "africa": null,
+                "config": {
+                    "primaryColor": "#000000"
+                }
+            },
+            "africa": {
+                "nickname": null,
+                "confName": null,
+                "country": "France"
+            }
+        },
+        {
+            "personId": 1627783,
+            "teamId": "12326",
+            "firstName": "Pascal",
+            "lastName": "Siakam",
+            "temporaryDisplayName": "Siakam, Pascal",
+            "heightMeters": "2.06",
+            "careerSummary": {
+                "fgp": "53.1",
+                "ppg": "10.1",
+                "rpg": "5.1",
+                "apg": "2",
+                "bpg": "0.6"
+            },
+            "teamSitesOnly": {
+                "posFull": "Forward"
+            },
+            "team": {
+                "teamId": "12326",
+                "africa": null,
+                "config": {
+                    "primaryColor": "#000000"
+                }
+            },
+            "africa": {
+                "nickname": null,
+                "confName": null,
+                "country": "Cameroon"
+            }
+        },
+        {
+            "personId": 202687,
+            "teamId": "12326",
+            "firstName": "Bismack",
+            "lastName": "Biyombo",
+            "temporaryDisplayName": "Biyombo, Bismack",
+            "heightMeters": "2.06",
+            "careerSummary": {
+                "fgp": "51.7",
+                "ppg": "4.9",
+                "rpg": "6.3",
+                "apg": "0.5",
+                "bpg": "1.4"
+            },
+            "teamSitesOnly": {
+                "posFull": "Center"
+            },
+            "team": {
+                "teamId": "12326",
+                "africa": null,
+                "config": {
+                    "primaryColor": "#000000"
+                }
+            },
+            "africa": {
+                "nickname": null,
+                "confName": null,
+                "country": "Democratic Republic of the Congo"
+            }
+        },
+        {
+            "personId": 201586,
+            "teamId": "12326",
+            "firstName": "Serge",
+            "lastName": "Ibaka",
+            "temporaryDisplayName": "Ibaka, Serge",
+            "heightMeters": "2.08",
+            "careerSummary": {
+                "fgp": "51.4",
+                "ppg": "12.3",
+                "rpg": "7.3",
+                "apg": "0.7",
+                "bpg": "2.2"
+            },
+            "teamSitesOnly": {
+                "posFull": "ForwardCenter"
+            },
+            "team": {
+                "teamId": "12326",
+                "africa": null,
+                "config": {
+                    "primaryColor": "#000000"
+                }
+            },
+            "africa": {
+                "nickname": null,
+                "confName": null,
+                "country": "Democratic Republic of the Congo"
+            }
+        },
+        {
+            "personId": 203954,
+            "teamId": "12326",
+            "firstName": "Joel",
+            "lastName": "Embiid",
+            "temporaryDisplayName": "Embiid, Joel",
+            "heightMeters": "2.13",
+            "careerSummary": {
+                "fgp": "48.1",
+                "ppg": "24.3",
+                "rpg": "11.4",
+                "apg": "3.2",
+                "bpg": "2"
+            },
+            "teamSitesOnly": {
+                "posFull": "Center"
+            },
+            "team": {
+                "teamId": "12326",
+                "africa": null,
+                "config": {
+                    "primaryColor": "#000000"
+                }
+            },
+            "africa": {
+                "nickname": null,
+                "confName": null,
+                "country": "Cameroon"
+            }
+        },
+        {
+            "personId": 200752,
+            "teamId": "12327",
+            "firstName": "Rudy",
+            "lastName": "Gay",
+            "temporaryDisplayName": "Gay, Rudy",
+            "heightMeters": "2.03",
+            "careerSummary": {
+                "fgp": "45.6",
+                "ppg": "17.6",
+                "rpg": "5.9",
+                "apg": "2.2",
+                "bpg": "0.8"
+            },
+            "teamSitesOnly": {
+                "posFull": "Forward"
+            },
+            "team": {
+                "teamId": "12327",
+                "africa": null,
+                "config": {
+                    "primaryColor": "#000000"
+                }
+            },
+            "africa": {
+                "nickname": null,
+                "confName": null,
+                "country": "USA"
+            }
+        }
+    ];
+
 
 app.consts = [];
 app.consts.url = [];
@@ -14,26 +318,42 @@ function GetData(year) {
         url: app.consts.url.GetPlayersProfilesTeamsConfigAll.format(year),
         data: {},
         beforeSend: function (xhr) {
-            // Load loader
+            // Empty content div
+            $("#content").empty();
+            // Show loader
+            $(".overlay").show();
         },
         success: function (result, status, xhr) {
             app.data = result;
             // Populate results
+            var successMessage = 'Success!';
+            console.log(successMessage);
+            // Hide loader.
+            $(".overlay").hide();
         },
         error: function (xhr, status, error) {
-            console.log('Error statusText: ' + xhr.statusText);
+            var errorText = 'Error statusText: ' + xhr.statusText;
+            console.log(errorText);
+            alert(errorText);
+            // Hide loader.
+            $(".overlay").hide();
         },
         complete: function (xhr, status) {
-            // Set unload loader.
+            var completeMessage = 'Complete!';
+            console.log(completeMessage);
+            /*alert(completeMessage);*/
+
+            // Hide loader.
+            $(".overlay").hide();
         }
     });
 }
 
 // Passing a named function instead of an anonymous function.
 function readyFn(jQuery) {
-    // Code to run when the document is ready.
+    // Code to run when the document is ready.    
     $("#divBtnApply").click(() => { alert("#divBtnApply" + " click!") });
-    
+    $(".overlay").hide();
 }
 
 
